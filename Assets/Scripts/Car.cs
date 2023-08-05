9f2df359bb8be8c20b56d57e9545745e0be0cec9
Yacine Betouche <goneyacine@gmail.com>
2023-08-05 12:17:35 +0100

    add linear regression

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -17,42 +17,64 @@ public class Car : MonoBehaviour
     private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
-        if (!File.Exists("C:\\Users\\TMAX27\\Desktop\\traindata.txt"))
-        {
-            string json = JsonConvert.SerializeObject(data);
-            File.WriteAllText("C:\\Users\\TMAX27\\Desktop\\traindata.txt", json);
-        }
+       if (!File.Exists("C:\\Users\\TMAX27\\Desktop\\traindata.json"))
+       {
+           string json = JsonConvert.SerializeObject(data);
+           File.WriteAllText("C:\\Users\\TMAX27\\Desktop\\traindata.json", json);
+       }
     }
     void FixedUpdate()
     {
-        Frame frame = new Frame();
-        cam.position = new Vector3 (transform.position.x, transform.position.y, cam.position.z);
+          Frame frame = new Frame();
+        cam.position = new Vector3(transform.position.x, transform.position.y, cam.position.z);
         Vector2 forwardDirection = (forward.position - transform.position) / Vector2.Distance(forward.position, transform.position);
         rb.velocity = forwardDirection * speed;
         float[] position = { transform.position.x, transform.position.y };
-        frame.position = position;
-        frame.angle = transform.eulerAngles.z;
-        float[,] hitPointsData = new float[12,2];
-        for(int i = 0; i < hitPoints.Length; i++)
-        {
-            hitPoints[i] = Physics2D.Raycast(transform.position, new Vector2(Mathf.Cos(3.14f / 6 * i), Mathf.Sin(3.14f / 6 * i))).point;
-            hitPointsData[i,0] = hitPoints[i].x;
-            hitPointsData[i,1] = hitPoints[i].y;
-            Debug.DrawRay(transform.position, new Vector2(Mathf.Cos(3.14f / 6 * i), Mathf.Sin(3.14f / 6 * i)) * Vector2.Distance(transform.position, hitPoints[i]), Color.red);
-        }
-        frame.hitPoints = hitPointsData;
-        frame.action = 0;
+         frame.position = position;
+         frame.angle = transform.eulerAngles.z;
+       float[,] hitPointsData = new float[12, 2];
+            frame.hitPoints = hitPointsData;
+           frame.action = 0;
         if (Input.GetKey(KeyCode.RightArrow))
         {
             Right();
-            frame.action = 1;
+               frame.action = 1;
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             Left();
-            frame.action = -1;
+                frame.action = -1;
         }
-        data.Add(frame);
+
+
+        float[] slopes = new float[] {
+    8.89215611e+03f, 1.84295013e+03f, 8.53961711e-04f, 3.55646525e-03f,
+    -6.40890912e+01f, 6.61378113e+02f, -1.14625326e+03f, -9.52341879e+02f,
+    5.50510392e+02f, -1.62963711e+02f, 1.38555305e-01f, -6.87682074e+02f,
+    -3.96057243e+02f, 2.33905862e+02f, 4.03904965e+02f, -5.27636043e-01f,
+    -3.23939741e+02f, -3.86561619e+02f, 6.72427727e+02f, 8.75795442e+02f,
+    -5.08120606e+02f, -6.51388783e+03f, 1.55525833e+01f, -2.02663177e+03f,
+    -1.16290592e+03f, 6.73577558e+01f, 1.15881188e+02f
+};
+          float inter = -0.13994918679163945f;
+
+         float action = transform.position.x * slopes[0] + transform.position.y * slopes[1] + transform.eulerAngles.z * slopes[2] + inter;
+
+        for (int i = 0; i < hitPoints.Length; i++)
+        {
+            hitPoints[i] = Physics2D.Raycast(transform.position, new Vector2(Mathf.Cos(3.14f / 6 * i), Mathf.Sin(3.14f / 6 * i))).point;
+            action += hitPoints[i].x * slopes[3 + (i * 2)] + hitPoints[i].y * slopes[3 + (i*2) + 1];
+          hitPointsData[i, 0] = hitPoints[i].x;
+         hitPointsData[i, 1] = hitPoints[i].y;
+            Debug.DrawRay(transform.position, new Vector2(Mathf.Cos(3.14f / 6 * i), Mathf.Sin(3.14f / 6 * i)) * Vector2.Distance(transform.position, hitPoints[i]), Color.red);
+        }
+
+        Debug.Log(action);
+        if (action >= 0.3f)
+            Right();
+        else if (action <= -0.3f)
+            Left();
+         //data.Add(frame);
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
@@ -60,17 +82,19 @@ public class Car : MonoBehaviour
         if (collision.collider.gameObject.tag == "Obsticall")
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-            string existingJson = File.ReadAllText("C:\\Users\\TMAX27\\Desktop\\traindata.txt");
-            List<Frame> existingData = new List<Frame>();
-            try
-            {
-                 existingData = JsonConvert.DeserializeObject<List<Frame>>("C:\\Users\\TMAX27\\Desktop\\traindata.txt");
-            }catch(Newtonsoft.Json.JsonReaderException e)
-            { 
-                
-            }
-            string json = JsonConvert.SerializeObject(existingData.Concat(data).ToList());
-            File.WriteAllText("C:\\Users\\TMAX27\\Desktop\\traindata.txt", json);
+           // if (data.Count > 1000)
+           // {
+           //
+           //     for (int i = 0; i < 200; i++)
+           //     {
+           //         data.Remove(data[data.Count - i -1]);
+           //     }
+           //     string existingJson = File.ReadAllText("C:\\Users\\TMAX27\\Desktop\\traindata.json");
+           //     List<Frame> existingData = JsonConvert.DeserializeObject<List<Frame>>(existingJson);
+           //     string json = JsonConvert.SerializeObject(existingData.Concat(data).ToList());
+           //     File.WriteAllText("C:\\Users\\TMAX27\\Desktop\\traindata.json", json);
+           //     Debug.Log(existingData.Count);
+           // }
         }
 
     }
